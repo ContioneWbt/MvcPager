27c0b6c7b0121f6db19fa0876879e42cdcfe854c
hecongcong <827422876@qq.com>
2019-05-16 09:57:20 +0800

    添加项目文件。

---
+++ PagerCheck/Extentions/PaginationEntity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PagerCheck.Extentions
+{
+    public class PaginationEntity<T> where T : class
+    {
+        public PaginationEntity(IEnumerable<T> source, Pagination pi)
+        {
+            this.Source = source.Skip((pi.PageIndex - 1) * pi.PageSize).Take(pi.PageSize);
+            this.Pi = pi;
+            this.Pi.ItemCount = source.Count();
+        }
+        public Pagination Pi { get; }
+        public IEnumerable<T> Source { get; set; }
+    }
+    public class Pagination
+    {
+     
+        public int ItemCount { get; set; }
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+    }
+}
---
+++ PagerCheck/Models/Title.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PagerCheck.Models
+{
+    public class Title
+    {
+        public string Name { get; set; }
+
+        public string Mz { get; set; }
+
+        public string CreateTime { get; set; }
+
+        public string GY { get; set; }
+    }
+}
